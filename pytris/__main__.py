1f9a0ac59e426c773647393f6bb2f374c23db673
Andrea92 <andrea.pontrandolfo@edu.itspiemonte.it>
2018-06-07 08:29:17 +0000

    [X] program should ask the name of the two players

--- pytris/__main__.py
+++ pytris/__main__.py
@@ -7,3 +7,9 @@ def main(args=None):
 
 if __name__ == '__main__':
     main(sys.argv[1:])
+
+
+player1 = input("Player1 Tell me your name \n")
+player2 = input("Player2 Tell me your name \n")
+
+print("Player1 is " + player1 + " and player2 is " + player2)
